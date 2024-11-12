6498e4deebe7421152ccb06d7e6bd787de4ae552
Paloma <154290084+palomapaldino5@users.noreply.github.com>
2024-11-12 10:32:22 -0300

    aula 1 material
    print, variavel e input

---
+++ python/aula1/aula1.py
@@ -0,0 +1,43 @@
+# AULA 1 - INTRODUÇÃO E VARIAVEIS
+
+# COMENTÁRIO
+
+"""
+DOCTRING - ANOTAÇÕES
+"""
+
+# PRINT - ESCREVER
+print("BOM DIA !!!")
+print("Paloma Paldino Melo")
+# CRLF - caracteres que representam
+# a quebra de do sistema
+#\r return
+#\n line feed
+
+# end (end - fim do print)
+print('Uma informação','importante', end='\n')
+# sep (sep - separador do print)
+print('Uma informação','importante', sep='----')
+print(123)
+
+#type
+print(type(11)) # int (inteiro) ex 1, 0
+print(type(1.5)) # float (numero flutuante, real) ex 1.5, -1.4
+print(type('oi'))# str (string, caractere) ex 'oi', "opa"
+print(type(True))# bool (boolean , logico) ex True , False
+
+# variavel 
+# 1 - caracteres especiais
+# 2 - espeços vazios
+#camelCase = umExemplo
+# snakeCase = um_exemplo
+# 3 - evite numeros solos, ou começo de variavel
+# *4 - evite nomes pouco descritivos
+
+# variaveis maiusculo são contantes
+# constante não ter seu valor mudado
+TESTE = 1
+teste = "teste1"
+testeLegal= 'TESTE'
+print(teste, teste, teste)
+
---
+++ python/aula1/exemploaula.py
@@ -0,0 +1,4 @@
+n1 = input('Qual é o primeiro número:')
+n2 = input ('qual é o segundo número:')
+s = int (n1) + int (n2) # '1' + '1'
+print('Soma dos números: ', s)
