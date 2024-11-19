7078de96fb6b091c1cfedf3efb562e5245b09fd7
palomapaldino5 <154290084+palomapaldino5@users.noreply.github.com>
2024-11-19 11:28:01 -0300

    atividade1
    lição em casa e treinar em casa.

---
+++ aula4 -listas/aula5 - funções/atividade1.py
@@ -0,0 +1,30 @@
+#1)
+def multiplica(x, y, z):
+       print(f'{x},{y},{z}/ {x*y*z}')
+        
+
+multiplica(6,9,5)
+
+
+
+multiplica(4,8,10)
+multiplica(y=4,z=8,x=10)
+
+
+#2) numeros par ou impar
+def numeros(n):
+    if n % 2 == 0:
+        print('PAR')
+    else:
+        print('IMPAR')
+
+numeros(5)
+numeros(8)
+
+#3) algoristmo para notas os alunos
+
+
+
+
+
+
