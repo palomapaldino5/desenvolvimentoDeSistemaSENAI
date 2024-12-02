8de124b6c2d05195748f9ec79496564669a6901e
palomapaldino5 <154290084+palomapaldino5@users.noreply.github.com>
2024-12-02 11:43:23 -0300

    atividade1

---
+++ aula13 - venv/atividade1.py
@@ -0,0 +1,10 @@
+"""
+1)Crie um ambiente virtual chamado calculadora_bolos e, dentro dele, um programa que calcule a quantidade de ingredientes necessários para assar 5 bolos, a partir de uma receita padrão. A receita padrão é:
+
+Farinha: 200g por bolo
+
+Açúcar: 100g por bolo
+
+Ovos: 2 por bolo
+"""
+
