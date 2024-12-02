6a1bffe85161c0f899f100605e74b3ff6f19ba11
palomapaldino5 <154290084+palomapaldino5@users.noreply.github.com>
2024-12-02 11:12:36 -0300

    aula13

--- aula12 - import/atividade2.py
+++ aula12 - import/atividade2.py
@@ -3,12 +3,12 @@
 """
 import random
 
-from random import exit as sair
+#from random import exit as sair
 
-import um_modelo_agenda
+#import um_modelo_agenda
 
-print(um_modelo_agenda)
-print(random)
+#print(um_modelo_agenda)
+#print(random)
 
 
 
---
+++ aula13 - venv/material.py
@@ -0,0 +1,34 @@
+"""
+CODIGO PARA PUXAR A VENV
+
+python -m venv venv
+
+ATIVAR VENV .\ endereço do activate
+
+DESCOBRIR QUAL PYTHON ESTOU ULTILIZANDO
+gcm python -Syntax
+
+LIMPAR TERMINAL
+cls
+
+INSTALAR PIP
+pip install
+
+INSTALAR 
+pip install pymysql
+
+DESINSTALAR PYMYSQL
+pip unistall pymysql
+
+VER BIBLIOTECAS INSTALADAS
+pip freeze
+
+CRIAR REQUIREMENTS
+pip freeze > requirements.txt
+
+INSTALAR REQUIREMENTS
+pip isntall -r requirements.txt
+
+DESATIVAR
+
+"""
