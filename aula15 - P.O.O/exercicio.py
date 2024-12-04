991c544f72a7a0c91ef61c0ea6dc114fc6f32ac6
palomapaldino5 <154290084+palomapaldino5@users.noreply.github.com>
2024-12-04 11:45:30 -0300

    exercicio15

---
+++ aula15 - P.O.O/exercicio.py
@@ -0,0 +1,8 @@
+class motorista:
+    while True:
+        def __init__(self, nome, idade, saldo,avaliacao):
+            self.nome = nome
+            self.idade = idade
+            self.saldo = saldo
+            self.avaliacao = avaliacao
+        
